5653abc1227bb7174ed8a757aeb791bca73fcd49
Redner_Notebook <aspidzmei132@gmail.com>
2026-04-22 13:00:40 +0400

    Создание, вывод, поиск узла в дереве. Не готов вывод внуков

---
+++ tree/tree_1.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+using namespace std;
+
+struct tree{
+    int inf;
+    tree *left;
+    tree *right;
+    tree *parent;
+};
+
+tree *node(int x){
+    tree *n = new tree;
+    n->inf = x;
+    n->parent = NULL;
+    n->right = NULL;
+    n->left = NULL;
+    return  n;
+}
+
+void postorder(tree *tr){ // обратный обход
+    if(tr){
+        postorder(tr->left);
+        postorder(tr->right);
+        cout << tr->inf << " ";
+    }
+}
+
+void inorder(tree *tr){ // обратный обход
+    if(tr){
+        inorder(tr->left);
+        cout << tr->inf << " ";
+        inorder(tr->right);
+    }
+}
+
+void insert(tree *&tr, int x){
+    tree *n = node(x);
+    if (!tr) {
+        tr = n;
+    }
+    else{
+        tree *y = tr;
+        while(y){
+            if(n->inf > y->inf){
+                if (y->right){
+                    y = y->right;
+                }
+                else{
+                    n->parent = y;
+                    y->right = n;
+                    break;
+                }
+            }
+            else if(n->inf < y->inf){
+                if (y->left){
+                    y = y->left;
+                }
+                else{
+                    n->parent = y;
+                    y->left = n;
+                    break;
+                }
+            }
+        }
+    }
+}
+
+tree *find(tree *tr, int x){//поиск
+    if (! tr || x == tr->inf){//нашли или дошли до конца ветки
+        return tr;
+    }
+    if (x < tr->inf){
+        return find(tr->left, x);//ищем по левой ветке
+    }
+    else{
+        return find(tr->right, x);//ищем по правой ветке
+    }
+}
+
+int main(){
+    setlocale(LC_ALL,"RUS");
+    int x, n;
+    cout << "Введите количество чисел: "; cin >> n;
+    tree *tr = NULL;
+    for(int i = 0; i < n; i++){
+        cout << i << ": ";
+        cin >> x;
+        insert(tr, x);
+    }
+    cout << "Введите x: "; cin >> x;
+    cout<< "Прямой обход: ";
+    inorder(tr);
+    cout << endl;
+    cout << "Внуки X: ";
+    postorder(find(tr, x));
+    return 0;
+}
